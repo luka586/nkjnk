ba0fd42f811088592c83b94eb4f5a4b1ee541a0d
luka586 <83034979+luka586@users.noreply.github.com>
2021-04-22 23:00:33 +0400

    Add files via upload

---
+++ leqtori1/leqtori1/Leqtori.cpp
@@ -0,0 +1,6 @@
+#include "Leqtori.h"
+#include<iostream>
+using namespace std;
+void Lectori::printLector() {
+	cout << name << " " << lname << " " << count << " " << status << endl;
+}
---
+++ leqtori1/leqtori1/Leqtori.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include<string>
+class Lectori
+{
+public:
+	string name, lname, status;
+	int count;
+	void printLector();
+};
